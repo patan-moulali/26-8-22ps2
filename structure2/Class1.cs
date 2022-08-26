799ff19945a802030b38747d2330ac4d788fb2a5
Patan Moulali <patan.moulali@mphasis.com>
2022-08-26 14:57:49 +0530

    Add project files.

---
+++ structure2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structure
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Student[] emp = { new  Student(),
+
+                       new  Student(),
+
+                       new  Student() };
+
+
+
+
+
+            emp[0].SetStudent(1, "moulali", 20);
+
+            emp[1].SetStudent(2, "apsar", 21);
+
+            emp[2].SetStudent(3, "nagendra", 23);
+
+
+
+
+            emp[0].DisplayStudent();
+
+            emp[1].DisplayStudent();
+
+            emp[2].DisplayStudent();
+
+        }
+    }
+
+
+}
+
+
---
+++ structure2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+public struct Student
+{
+
+
+
+
+    public int Id;
+
+    public string Name;
+
+    public int Age;
+
+
+
+
+
+    public void SetStudent(int id, string name, int age)
+
+    {
+
+        Id = id;
+
+        Name = name;
+
+        Age = age;
+
+
+    }
+
+
+
+
+    public void DisplayStudent()
+
+    {
+
+        Console.WriteLine(" Student:");
+
+        Console.WriteLine("\tId    : " + Id);
+
+        Console.WriteLine("\tName   : " + Name);
+
+        Console.WriteLine("\tAge   : " + Age);
+
+        Console.WriteLine("\n");
+        Console.ReadLine();
+
+    }
+
+
+
+}
